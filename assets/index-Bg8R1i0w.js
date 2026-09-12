01378961b81a410b2e091a3224c68496357962e8
zkj2356483557 <177946489+zkj2356483557@users.noreply.github.com>
2026-09-12 19:40:22 +0800

    deploy: 原生外壳适配 + FAB 遮挡修复 + PWA 图标

---
+++ assets/index-Bg8R1i0w.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["./web-CSkDv3FU.js","./index-CUhkQdGr.js","./index-DmJGs4ZU.css"])))=>i.map(i=>d[i]);
+import{r,_ as t}from"./index-CUhkQdGr.js";const _=r("SplashScreen",{web:()=>t(()=>import("./web-CSkDv3FU.js"),__vite__mapDeps([0,1,2]),import.meta.url).then(e=>new e.SplashScreenWeb)});export{_ as SplashScreen};
---
+++ assets/index-WOPrChOn.js
@@ -0,0 +1,1 @@
+import{r as n}from"./index-CUhkQdGr.js";var a;(function(t){t.Dark="DARK",t.Light="LIGHT",t.Default="DEFAULT"})(a||(a={}));var r;(function(t){t.None="NONE",t.Slide="SLIDE",t.Fade="FADE"})(r||(r={}));const s=r,u=a,e=n("StatusBar");export{r as Animation,e as StatusBar,s as StatusBarAnimation,u as StatusBarStyle,a as Style};
